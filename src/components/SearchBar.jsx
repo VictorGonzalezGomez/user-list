374a77d58aeeaa6e6351f14c82ef6cc6de5ef9bd
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-09-03 01:10:03 -0400

    hotfix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React ,{useEffect}from "react";
+import React from "react";
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
